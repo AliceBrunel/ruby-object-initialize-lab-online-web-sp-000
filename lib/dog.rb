990e5e2a2c23581df7ed28ccff22c8b5b0ef340e
Alice Brunel <alc.brunel@gmail.com>
2019-07-14 17:32:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,17 @@
 class Dog 
   attr_accessor :name
-  attr_accessor :breed
   
   def initialize(name,breed)
     @name = name
-    @breed = breed
-  end
-  
-  def breed(dog_breed)
     @breed = "Mutt"
   end
   
   def breed(dog_breed)
-    @breed = "Mutt"
+    @breed = dog_breed
+  end
+  
+  def breed
+    @breed
   end
   
 end
